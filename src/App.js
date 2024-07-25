27c56aa43a379ff24fd0846700ebe95fa79994c4
Ali <ali@Developer.local>
2024-07-26 00:31:49 +0330

    finish

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from "./component/Home"
 import Logout from "./component/Logout"
 import Login from "./component/Login"
@@ -13,7 +13,6 @@ import Editcon from "./component/Editcon"
 
 function App() {
   return (
-
     <Router>
       <Routes>
         <Route path='/contacts' Component={Home} />
@@ -22,7 +21,10 @@ function App() {
         <Route path='/logout' Component={Logout} />
         <Route path='/crate' Component={Crate1} />
         <Route path='/Edit' Component={Editcon} />
-
+        <Route
+          path="*"
+          element={<Navigate to="/contacts" replace={true} />}
+        />
       </Routes>
     </Router>
   );
